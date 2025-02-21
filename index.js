a7634e436300ede3a8634ea1718c3567e6334956
sajid-edninja <ssk73573@gmail.com>
2025-02-21 08:53:11 +0530

    add signup logic

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const app = express();
 
 app.use(express.json());
 
+let users = {};
+
+app.post("/signup", (req, res) => {
+  if (users[req.body.username]) {
+    return res.status(400).json({
+      message: "User already exists, please sign in.",
+    });
+  }
+  const token = "sdfksfhsdff94df";
+  users[req.body.username] = {
+    username: req.body.username,
+    password: req.body.password,
+    token: token,
+  };
+  return res.status(200).json({
+    token,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
 });
