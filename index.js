b6c651d766bc122fdf937e66d044c1b4b570b87a
sajid-edninja <ssk73573@gmail.com>
2025-02-21 08:53:31 +0530

    add signin logic

--- index.js
+++ index.js
@@ -23,6 +23,22 @@ app.post("/signup", (req, res) => {
   });
 });
 
+app.post("/signin", (req, res) => {
+  if (!users[req.body.username]) {
+    return res.status(404).json({
+      message: "User not found. Please sign up first.",
+    });
+  }
+  if (users[req.body.username].password !== req.body.password) {
+    return res.status(400).json({
+      message: "Wrong password.",
+    });
+  }
+  return res.status(200).json({
+    token: users[req.body.username].token,
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
 });
