7b158959d75436eaee17f65b5aaf5c19875a1c90
sajid-edninja <ssk73573@gmail.com>
2025-02-22 09:30:49 +0530

    create auth middleware

---
+++ auth-middleware.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(express.json());
+
+function auth(req, res, next) {
+  try {
+    const token = req.headers.token;
+    if (!token) {
+      return res.status(400).json({ message: "Missing token in header." });
+    }
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+    req.body.username = decodedInfo.username;
+    next();
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid token." });
+  }
+}
+
+const JWT_SECRET = "sajidlovesspiderman";
+let users = [];
+
+app.post("/signup", (req, res) => {
+  const existingUser = users.find((user) => {
+    return user.username === req.body.username;
+  });
+  if (existingUser) {
+    return res.status(400).json({
+      message: "User already exists, please sign in.",
+    });
+  }
+  const token = jwt.sign(
+    {
+      username: req.body.username,
+    },
+    JWT_SECRET
+  );
+  users.push({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  return res.status(200).json({
+    token,
+  });
+});
+
+app.post("/signin", (req, res) => {
+  const existingUser = users.find((user) => {
+    return (
+      user.username === req.body.username && user.password === req.body.password
+    );
+  });
+  if (!existingUser) {
+    return res.status(400).json({
+      message: "Invalid username or password.",
+    });
+  }
+  const token = jwt.sign(
+    {
+      username: existingUser.username,
+    },
+    JWT_SECRET
+  );
+  return res.status(200).json({
+    token,
+  });
+});
+
+app.get("/user-profile", auth, (req, res) => {
+  const foundUser = users.find((user) => user.username === req.body.username);
+  if (!foundUser) {
+    return res.status(400).json({
+      message: "Invalid token.",
+    });
+  }
+  return res.status(200).json({
+    username: foundUser.username,
+    password: foundUser.password,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000.");
+});
