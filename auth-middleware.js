1d48aad1b04dd8fa5f0a2adfb25ab444ada43bb8
sajid-edninja <ssk73573@gmail.com>
2025-02-22 09:52:43 +0530

    add cors

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 function auth(req, res, next) {
   try {
