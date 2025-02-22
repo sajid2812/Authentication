811e250add0225eddb727c19d40147f23c6f2126
sajid-edninja <ssk73573@gmail.com>
2025-02-22 13:20:01 +0530

    webpage fix

--- auth-middleware.js
+++ auth-middleware.js
@@ -86,6 +86,6 @@ app.get("/user-profile", auth, (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000.");
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080.");
 });
